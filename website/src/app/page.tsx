8cda43a4b80e89155dbfc88def0e7566a8a87d96
sabatofrancesco <amministrazione@mvmpackaging.com>
2026-09-07 16:14:51 +0200

    Add product categories to homepage

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { WhyMVM } from "@/components/WhyMVM";
+import { ProductCategories } from "@/components/ProductCategories";
 import { FeaturedCollection } from "@/components/FeaturedCollection";
 import { DevelopmentProcess } from "@/components/DevelopmentProcess";
 import { FinalCTA } from "@/components/FinalCTA";
 import { Footer } from "@/components/Footer";
 
-export default function Home(){return <main><Header/><Hero/><WhyMVM/><FeaturedCollection/><DevelopmentProcess/><FinalCTA/><Footer/></main>}
+export default function Home(){return <main><Header/><Hero/><WhyMVM/><ProductCategories/><FeaturedCollection/><DevelopmentProcess/><FinalCTA/><Footer/></main>}
